0a8aa2ed9e207c6bb3e2952736187cf259035486
GatitoDeInvierno <89356559+danikdragon@users.noreply.github.com>
2024-07-29 17:59:20 +0500

    Карта
    убрал главный уровень в отдельный
    появилось кучу комнат
    появилась рандомная генерация уровня

---
+++ Source/TestSubject/CreateLevel.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CreateLevel.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TESTSUBJECT_API UCreateLevel : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UCreateLevel();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
